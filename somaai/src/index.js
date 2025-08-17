495aea534d4885b23a1687effce8e751e74eadd6
Scoare <danielklaferriere@gmail.com>
2025-08-16 20:27:55 -0400

    Added React Routing

--- somaai/src/index.js
+++ somaai/src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Lessons from './Lessons';
+import LessonPage from './LessonPage'
+import AIPage from './AIPage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="lessons" element={<Lessons />} />
+        <Route path='lesson/:id' element={<LessonPage />} />
+        <Route path='*' element={<h1>Error 404: Page not found.</h1>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
